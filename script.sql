550c8b747ad8d9b05e8a83f2ab84732d28dfd741
David <davidwashington833@gmail.com>
2018-03-22 00:49:49 -0300

    Gerando script de importação

--- script.sql
+++ script.sql
@@ -1,119 +1,398 @@
-CREATE DATABASE pi5;
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 22-Mar-2018 às 04:47
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
-USE pi5;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE usuario (
-  idUsuario INT NOT NULL AUTO_INCREMENT,
-  nomeUsuario VARCHAR(50) NOT NULL,
-  sobrenomeUsuario VARCHAR(50) NOT NULL,
-  emailUsuario VARCHAR(50) NOT NULL,
-  senhaUsuario VARCHAR(255) NOT NULL,
-  CPFUsuario VARCHAR(50) NOT NULL,
-  celularUsuario VARCHAR(50) NOT NULL,
-  tokenCartaoUsuario VARCHAR(255) NOT NULL,
-  nascimentoUsuario DATETIME NOT NULL,
-  PRIMARY KEY (idUsuario)
-)
 
-CREATE TABLE endereco (
-  idEndereco INT NOT NULL AUTO_INCREMENT,
-  ruaEndereco VARCHAR(50) NOT NULL,
-  logradouroEndereco VARCHAR(50) NOT NULL,
-  complementoEndereco VARCHAR(50) NOT NULL,
-  estadoEndereco VARCHAR(50) NOT NULL,
-  cidadeEndereco VARCHAR(50) NOT NULL,
-  numeroEndereco INT NOT NULL,
-  CEPEndereco INT NOT NULL,
-  idUsuario INT NOT NULL,
-  FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-  PRIMARY KEY (idEndereco)
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE dadosBancario (
-  idDadosBancario INT NOT NULL AUTO_INCREMENT,
-  contaDadosBancario INT NOT NULL,
-  agenciaDadosBancario INT NOT NULL,
-  tipoDocumentoDadosBancario VARCHAR(4),
-  documentoDadosBancario INT NOT NULL,
-  PRIMARY KEY (idDadosBancario)
-)
+--
+-- Database: `pi5`
+--
 
-CREATE TABLE prestador (
-  idPrestador INT NOT NULL AUTO_INCREMENT,
-  latitudePrestador BIGINT,
-  longitudePrestador BIGINT,
-  idUsuario INT NOT NULL,
-  idEndereco INT NOT NULL,
-  idDadosBancario INT NOT NULL,
-  FOREIGN KEY (idDadosBancario) REFERENCES dadosBancario(idDadosBancario),
-  FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-  FOREIGN KEY (idEndereco) REFERENCES endereco(idEndereco),
-  PRIMARY KEY (idPrestador)
-)
+-- --------------------------------------------------------
 
-CREATE TABLE categoria (
-  idCategoria INT NOT NULL AUTO_INCREMENT,
-  nomeCategoria VARCHAR(50) NOT NULL,
-  PRIMARY KEY (idCategoria)
-)
+--
+-- Estrutura da tabela `anuncio`
+--
 
-CREATE TABLE servico (
-  idServico INT NOT NULL AUTO_INCREMENT,
-  descricaoServico VARCHAR(255) NOT NULL,
-  imageServico VARCHAR(255) NOT NULL,
-  precoServico DECIMAL(15,2) NOT NULL,
-  idPrestador INT NOT NULL,
-  idCategoria INT NOT NULL,
-  FOREIGN KEY (idPrestador) REFERENCES prestador(idPrestador),
-  FOREIGN KEY (idCategoria) REFERENCES categoria(idCategoria),
-  PRIMARY KEY (idServico)
-)
+CREATE TABLE `anuncio` (
+  `idAnuncio` int(11) NOT NULL,
+  `tituloAnuncio` varchar(50) NOT NULL,
+  `descricaoAnuncio` varchar(255) NOT NULL,
+  `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE avaliacao (
-  idAvaliacao INT NOT NULL AUTO_INCREMENT,
-  pontualidadeAvaliacao INT NOT NULL,
-  qualidadeAvaliacao INT NOT NULL,
-  precoAvaliacao INT NOT NULL,
-  idUsuario INT NOT NULL,
-  idPrestador INT NOT NULL,
-  FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-  FOREIGN KEY (idPrestador) REFERENCES prestador(idPrestador),
-  PRIMARY KEY (idAvaliacao)
-)
+-- --------------------------------------------------------
 
-CREATE TABLE anuncio (
-  idAnuncio INT NOT NULL AUTO_INCREMENT,
-  tituloAnuncio VARCHAR(50) NOT NULL,
-  descricaoAnuncio VARCHAR(255) NOT NULL,
-  idUsuario INT NOT NULL,
-  FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-  PRIMARY KEY (idAnuncio)
-)
+--
+-- Estrutura da tabela `avaliacao`
+--
 
-CREATE TABLE transacao (
-  idTransacao INT NOT NULL AUTO_INCREMENT,
-  idUsuario INT NOT NULL,
-  idPrestador INT NOT NULL,
-  valor DECIMAL(15,2),
-  FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-  FOREIGN KEY (idPrestador) REFERENCES prestador(idPrestador),
-  PRIMARY KEY (idTransacao)
-)
+CREATE TABLE `avaliacao` (
+  `idAvaliacao` int(11) NOT NULL,
+  `pontualidadeAvaliacao` int(11) NOT NULL,
+  `qualidadeAvaliacao` int(11) NOT NULL,
+  `precoAvaliacao` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idPrestador` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE pedido (
-  idPedido INT NOT NULL AUTO_INCREMENT,
-  idUsuario INT NOT NULL,
-  idPrestador INT NOT NULL,
-  FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-  FOREIGN KEY (idPrestador) REFERENCES prestador(idPrestador),
-  PRIMARY KEY (idPedido)
-)
+-- --------------------------------------------------------
 
-CREATE TABLE itemPedido (
-  idItemPedido INT NOT NULL AUTO_INCREMENT,
-  idPedido INT NOT NULL,
-  idServico INT NOT NULL,
-  FOREIGN KEY (idPedido) REFERENCES pedido(idPedido),
-  FOREIGN KEY (idServico) REFERENCES servico(idServico),
-  PRIMARY KEY (idItemPedido)
-)
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nomeCategoria` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `dadosbancario`
+--
+
+CREATE TABLE `dadosbancario` (
+  `idDadosBancario` int(11) NOT NULL,
+  `contaDadosBancario` int(11) NOT NULL,
+  `agenciaDadosBancario` int(11) NOT NULL,
+  `tipoDocumentoDadosBancario` varchar(4) DEFAULT NULL,
+  `documentoDadosBancario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `idEndereco` int(11) NOT NULL,
+  `ruaEndereco` varchar(50) NOT NULL,
+  `logradouroEndereco` varchar(50) NOT NULL,
+  `complementoEndereco` varchar(50) NOT NULL,
+  `estadoEndereco` varchar(50) NOT NULL,
+  `cidadeEndereco` varchar(50) NOT NULL,
+  `numeroEndereco` int(11) NOT NULL,
+  `CEPEndereco` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itempedido`
+--
+
+CREATE TABLE `itempedido` (
+  `idItemPedido` int(11) NOT NULL,
+  `idPedido` int(11) NOT NULL,
+  `idServico` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idPedido` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idPrestador` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `prestador`
+--
+
+CREATE TABLE `prestador` (
+  `idPrestador` int(11) NOT NULL,
+  `latitudePrestador` bigint(20) DEFAULT NULL,
+  `longitudePrestador` bigint(20) DEFAULT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idEndereco` int(11) NOT NULL,
+  `idDadosBancario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `idServico` int(11) NOT NULL,
+  `descricaoServico` varchar(255) NOT NULL,
+  `imageServico` varchar(255) NOT NULL,
+  `precoServico` decimal(15,2) NOT NULL,
+  `idPrestador` int(11) NOT NULL,
+  `idCategoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `transacao`
+--
+
+CREATE TABLE `transacao` (
+  `idTransacao` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idPrestador` int(11) NOT NULL,
+  `valor` decimal(15,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nomeUsuario` varchar(50) NOT NULL,
+  `sobrenomeUsuario` varchar(50) NOT NULL,
+  `emailUsuario` varchar(50) NOT NULL,
+  `senhaUsuario` varchar(255) NOT NULL,
+  `CPFUsuario` varchar(50) NOT NULL,
+  `celularUsuario` varchar(50) NOT NULL,
+  `tokenCartaoUsuario` varchar(255) NOT NULL,
+  `nascimentoUsuario` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anuncio`
+--
+ALTER TABLE `anuncio`
+  ADD PRIMARY KEY (`idAnuncio`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indexes for table `avaliacao`
+--
+ALTER TABLE `avaliacao`
+  ADD PRIMARY KEY (`idAvaliacao`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idPrestador` (`idPrestador`);
+
+--
+-- Indexes for table `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idCategoria`);
+
+--
+-- Indexes for table `dadosbancario`
+--
+ALTER TABLE `dadosbancario`
+  ADD PRIMARY KEY (`idDadosBancario`);
+
+--
+-- Indexes for table `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`idEndereco`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indexes for table `itempedido`
+--
+ALTER TABLE `itempedido`
+  ADD PRIMARY KEY (`idItemPedido`),
+  ADD KEY `idPedido` (`idPedido`),
+  ADD KEY `idServico` (`idServico`);
+
+--
+-- Indexes for table `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idPrestador` (`idPrestador`);
+
+--
+-- Indexes for table `prestador`
+--
+ALTER TABLE `prestador`
+  ADD PRIMARY KEY (`idPrestador`),
+  ADD KEY `idDadosBancario` (`idDadosBancario`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idEndereco` (`idEndereco`);
+
+--
+-- Indexes for table `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`idServico`),
+  ADD KEY `idPrestador` (`idPrestador`),
+  ADD KEY `idCategoria` (`idCategoria`);
+
+--
+-- Indexes for table `transacao`
+--
+ALTER TABLE `transacao`
+  ADD PRIMARY KEY (`idTransacao`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idPrestador` (`idPrestador`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anuncio`
+--
+ALTER TABLE `anuncio`
+  MODIFY `idAnuncio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `avaliacao`
+--
+ALTER TABLE `avaliacao`
+  MODIFY `idAvaliacao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dadosbancario`
+--
+ALTER TABLE `dadosbancario`
+  MODIFY `idDadosBancario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `idEndereco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `itempedido`
+--
+ALTER TABLE `itempedido`
+  MODIFY `idItemPedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prestador`
+--
+ALTER TABLE `prestador`
+  MODIFY `idPrestador` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `idServico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transacao`
+--
+ALTER TABLE `transacao`
+  MODIFY `idTransacao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `anuncio`
+--
+ALTER TABLE `anuncio`
+  ADD CONSTRAINT `anuncio_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Limitadores para a tabela `avaliacao`
+--
+ALTER TABLE `avaliacao`
+  ADD CONSTRAINT `avaliacao_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `avaliacao_ibfk_2` FOREIGN KEY (`idPrestador`) REFERENCES `prestador` (`idPrestador`);
+
+--
+-- Limitadores para a tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD CONSTRAINT `endereco_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Limitadores para a tabela `itempedido`
+--
+ALTER TABLE `itempedido`
+  ADD CONSTRAINT `itempedido_ibfk_1` FOREIGN KEY (`idPedido`) REFERENCES `pedido` (`idPedido`),
+  ADD CONSTRAINT `itempedido_ibfk_2` FOREIGN KEY (`idServico`) REFERENCES `servico` (`idServico`);
+
+--
+-- Limitadores para a tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`idPrestador`) REFERENCES `prestador` (`idPrestador`);
+
+--
+-- Limitadores para a tabela `prestador`
+--
+ALTER TABLE `prestador`
+  ADD CONSTRAINT `prestador_ibfk_1` FOREIGN KEY (`idDadosBancario`) REFERENCES `dadosbancario` (`idDadosBancario`),
+  ADD CONSTRAINT `prestador_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `prestador_ibfk_3` FOREIGN KEY (`idEndereco`) REFERENCES `endereco` (`idEndereco`);
+
+--
+-- Limitadores para a tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD CONSTRAINT `servico_ibfk_1` FOREIGN KEY (`idPrestador`) REFERENCES `prestador` (`idPrestador`),
+  ADD CONSTRAINT `servico_ibfk_2` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`);
+
+--
+-- Limitadores para a tabela `transacao`
+--
+ALTER TABLE `transacao`
+  ADD CONSTRAINT `transacao_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `transacao_ibfk_2` FOREIGN KEY (`idPrestador`) REFERENCES `prestador` (`idPrestador`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
